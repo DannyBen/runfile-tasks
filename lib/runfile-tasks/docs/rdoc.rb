7adee3337e715c2c3d761f88892040de40d6a9ff
DannyB <dannyb@telesofia.com>
2016-04-08 18:38:04 +0000

    convert some tabs to spaces

--- lib/runfile-tasks/docs/rdoc.rb
+++ lib/runfile-tasks/docs/rdoc.rb
@@ -1,25 +1,25 @@
 module RunfileTasks
-	module Docs
-		extend self
+  module Docs
+    extend self
 
-		@@default_rdoc_options = [
-			"--main README.md",
-			"--all",
-		]
+    @@default_rdoc_options = [
+      "--main README.md",
+      "--all",
+    ]
 
-		def rdoc(files=nil, options=@@default_rdoc_options)
-			files or files = Dir['**/*.{rb,md}']
-			files = "'" + files.join("' '") + "'"
-			usage  "rdoc [-- <options>...]"
-			help   "Generate documentation using the rdoc command line tool. To pass arguments to rdoc, place them after '--'."
-			action :rdoc do |args|
-				inopts = args['<options>']
-				options = inopts unless inopts.empty?
-				options = options.join(' ')
-				cmd = "rdoc #{options} #{files}"
-				say "!txtgrn!Running: !txtpur!#{cmd}"
-				system cmd
-			end
-		end
-	end
+    def rdoc(files=nil, options=@@default_rdoc_options)
+      files or files = Dir['**/*.{rb,md}']
+      files = "'" + files.join("' '") + "'"
+      usage  "rdoc [-- <options>...]"
+      help   "Generate documentation using the rdoc command line tool. To pass arguments to rdoc, place them after '--'."
+      action :rdoc do |args|
+        inopts = args['<options>']
+        options = inopts unless inopts.empty?
+        options = options.join(' ')
+        cmd = "rdoc #{options} #{files}"
+        say "!txtgrn!Running: !txtpur!#{cmd}"
+        system cmd
+      end
+    end
+  end
 end
--- lib/runfile-tasks/testing/minitest.rb
+++ lib/runfile-tasks/testing/minitest.rb
@@ -1,23 +1,23 @@
 module RunfileTasks
-	module Testing
-		extend self
+  module Testing
+    extend self
 
-		def minitest(pattern="./test/*_test.rb")
-			usage  "test [<name>]"
-			help   "Run all tests or a single test file."
-			action :test do |args|
-				if args['<name>'] 
-					file = pattern.sub "*", args['<name>']
-					say "!txtgrn!Using: !txtpur!#{file}"
-					require file
-				else
-					Dir[pattern].each do |file| 
-						say "!txtgrn!Using: !txtpur!#{file}"
-						require file
-					end
-				end
-			end
-		end
+    def minitest(pattern="./test/*_test.rb")
+      usage  "test [<name>]"
+      help   "Run all tests or a single test file."
+      action :test do |args|
+        if args['<name>'] 
+          file = pattern.sub "*", args['<name>']
+          say "!txtgrn!Using: !txtpur!#{file}"
+          require file
+        else
+          Dir[pattern].each do |file| 
+            say "!txtgrn!Using: !txtpur!#{file}"
+            require file
+          end
+        end
+      end
+    end
 
-	end
+  end
 end
