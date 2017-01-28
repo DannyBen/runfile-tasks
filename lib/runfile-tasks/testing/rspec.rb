1b70cb662d4176c433b5c89c50856eb6e79d6ea4
DannyB <dannyb@telesofia.com>
2017-01-28 10:35:29 +0200

    rspec: add ability to run tag only

--- lib/runfile-tasks/testing/rspec.rb
+++ lib/runfile-tasks/testing/rspec.rb
@@ -12,18 +12,30 @@ module RunfileTasks
       }.merge opts
 
       usage  "#{opts[:action]} [<name>] [<tag>]"
-      help   "Run all specs or a single spec file matching a regex. You can provide a tag to run only specific tests."
+      help   "Run all specs or a single spec file matching a regex. You can provide a tag to run only specific tests. If you wish to provide a tag only, without a file pattern, simply prefix the tag with a colon (like 'run spec :focus')"
       action opts[:action].to_sym do |args|
-        if args['<name>'] 
+        file = args['<name>'] 
+        tag  = args['<tag>'] 
+
+        if file and file[0] == ':'
+          tag = file
+          file = nil
+        end
+
+        if tag and tag[0] == ':'
+          tag = tag[1..-1]
+        end
+
+        if file
           files = Dir[opts[:pattern]]  
-          files.select! { |file| file =~ /#{args['<name>']}/i }
+          files.select! { |f| f =~ /#{file}/i }
           abort "Cannot find a matching spec file with #{opts[:pattern]}" if files.empty?
           file = files.first
           cmd = "#{opts[:command]} \"#{file}\""
         else
           cmd = "#{opts[:command]}"
         end
-        cmd = "#{cmd} --tag #{args['<tag>']}" if args['<tag>']
+        cmd = "#{cmd} --tag #{tag}" if tag
         say "!txtgrn!Running: !txtpur!#{cmd}"
         system cmd
       end
