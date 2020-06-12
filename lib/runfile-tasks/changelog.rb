11389e2e0f11a370be72c587834347564ed76721
Danny Ben Shitrit <db@dannyben.com>
2020-06-12 06:52:14 +0000

    add changelog task

---
+++ lib/runfile-tasks/changelog.rb
@@ -0,0 +1,18 @@
+module RunfileTasks
+  module Changelog
+    extend self
+
+    def generator(repo)
+      usage  "changelog [--commit]"
+      help   "Generate changelog"
+      option "-c, --commit", "Also commit CHANGELOG.md"
+      action :changelog do |args|
+        user, project = repo.split "/"
+        commit = args['--commit'] || args['--push']
+
+        run "github_changelog_generator --project #{project} --user #{user}"
+        run "git add CHANGELOG.md && git commit -am 'update changelog'" if commit
+      end
+    end
+  end
+end
