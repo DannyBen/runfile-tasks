3560675423d9a808a9e33660ba2da20ccf7a6a76
Danny Ben Shitrit <db@dannyben.com>
2022-11-29 14:44:33 +0000

    add --next to denote --next-failure for rspec

--- lib/runfile-tasks/testing/rspec.rb
+++ lib/runfile-tasks/testing/rspec.rb
@@ -14,11 +14,14 @@ module RunfileTasks
         command: 'rspec'
       }.merge opts
 
-      usage  opts[:action] == :global ? "[NAME] [TAG]" : "#{opts[:action]} [NAME] [TAG]"
+      usage_text = "[NAME] [TAG] [--next]"
+      usage  opts[:action] == :global ? usage_text : "#{opts[:action]} #{usage_text}"
       help   "Run all specs or a single spec file matching a regex. You can provide a tag to run only specific tests. If you wish to provide a tag only, without a file pattern, simply prefix the tag with a colon, like 'run spec :focus'"
+      option "-n --next", "Run with --next-failure"
       action opts[:action].to_sym do |args|
         file = args['NAME'] 
-        tag  = args['TAG'] 
+        tag = args['TAG'] 
+        next_failure = args['--next']
 
         if file and file[0] == ':'
           tag = file
@@ -39,6 +42,7 @@ module RunfileTasks
           cmd = "#{opts[:command]}"
         end
         cmd = "#{cmd} --tag #{tag}" if tag
+        cmd = "#{cmd} --next-failure" if next_failure
         puts "g`Running:` p`#{cmd}`".in_color
         exec cmd
       end
